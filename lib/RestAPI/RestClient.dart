c73df841110efca8870cd6b2c8a2f4134ff1db24
Abu Noman Basar <abunomanbasar.eappair@gmail.com>
2024-12-31 16:31:47 +0600

    dfgds

--- lib/RestAPI/RestClient.dart
+++ lib/RestAPI/RestClient.dart
@@ -3,79 +3,73 @@ import 'package:http/http.dart' as http;
 
 import '../Style/Style.dart';
 
-Future<bool> ProductCreateRequest(Formvalues) async{
-  var URL=Uri.parse("https://crud.teamrabbil.com/api/v1/CreateProduct");
-  var PostBody=json.encode(Formvalues);
-  var PostHeader={"Content-Type" : "application/json"};
-  var Response = await http.post(URL,headers: PostHeader, body: PostBody);
-  var ResultCode=Response.statusCode;
-  var ResultBody=json.decode(Response.body);
+Future<bool> ProductCreateRequest(Formvalues) async {
+  var URL = Uri.parse("https://crud.teamrabbil.com/api/v1/CreateProduct");
+  var PostBody = json.encode(Formvalues);
+  var PostHeader = {"Content-Type": "application/json"};
+  var Response = await http.post(URL, headers: PostHeader, body: PostBody);
+  var ResultCode = Response.statusCode;
+  var ResultBody = json.decode(Response.body);
 
-  if(ResultCode==200 && ResultBody['status']=="success"){
+  if (ResultCode == 200 && ResultBody['status'] == "success") {
     SuccessToast("Request Success");
     return true;
-  }
-  else{
+  } else {
     ErrorToast("Request failed ! Try again");
     return false;
   }
 }
 
-Future<bool> ProductUpdateRequest(Formvalues,id) async{
-  var URL=Uri.parse("https://crud.teamrabbil.com/api/v1/UpdateProduct/"+id);
-  var PostBody=json.encode(Formvalues);
-  var PostHeader={"Content-Type" : "application/json"};
-  var Response = await http.post(URL,headers: PostHeader, body: PostBody);
-  var ResultCode=Response.statusCode;
-  var ResultBody=json.decode(Response.body);
+Future<bool> ProductUpdateRequest(Formvalues, id) async {
+  var URL = Uri.parse("https://crud.teamrabbil.com/api/v1/UpdateProduct/" + id);
+  var PostBody = json.encode(Formvalues);
+  var PostHeader = {"Content-Type": "application/json"};
+  var Response = await http.post(URL, headers: PostHeader, body: PostBody);
+  var ResultCode = Response.statusCode;
+  var ResultBody = json.decode(Response.body);
 
-  if(ResultCode==200 && ResultBody['status']=="success"){
+  if (ResultCode == 200 && ResultBody['status'] == "success") {
     SuccessToast("Request Success");
     return true;
-  }
-  else{
+  } else {
     ErrorToast("Request failed ! Try again");
     return false;
   }
 }
 
-Future<List> ProductGridViewList() async{
-  var URL=Uri.parse("https://crud.teamrabbil.com/api/v1/ReadProduct");
-  var PostHeader={"Content-Type" : "application/json"};
-  var Response=await http.get(URL,headers: PostHeader);
+Future<List> ProductGridViewList() async {
+  var URL = Uri.parse("https://crud.teamrabbil.com/api/v1/ReadProduct");
+  var PostHeader = {"Content-Type": "application/json"};
+  var Response = await http.get(URL, headers: PostHeader);
 
-  var ResultCode=Response.statusCode;
-  var ResultBody=json.decode(Response.body);
+  var ResultCode = Response.statusCode;
+  var ResultBody = json.decode(Response.body);
 
-  if(ResultCode==200 && ResultBody['status']=="success"){
+  if (ResultCode == 200 && ResultBody['status'] == "success") {
     SuccessToast("Request Success");
     return ResultBody['data'];
-  }
-  else{
+  } else {
     ErrorToast("Request Failed! Try Again");
     return [];
   }
 }
 
-Future<bool> ProductDeleteRequest(id)async{
-  var URL=Uri.parse("https://crud.teamrabbil.com/api/v1/DeleteProduct/"+id);
-  var PostHeader={"Content-Type" : "application/json"};
-  var Response=await http.get(URL,headers: PostHeader);
+Future<bool> ProductDeleteRequest(id) async {
+  var URL = Uri.parse("https://crud.teamrabbil.com/api/v1/DeleteProduct/" + id);
+  var PostHeader = {"Content-Type": "application/json"};
+  var Response = await http.get(URL, headers: PostHeader);
 
-  var ResultCode=Response.statusCode;
-  var ResultBody=json.decode(Response.body);
+  var ResultCode = Response.statusCode;
+  var ResultBody = json.decode(Response.body);
 
-  if(ResultCode==200 && ResultBody['status']=="success"){
+  if (ResultCode == 200 && ResultBody['status'] == "success") {
     SuccessToast("Request Success");
     return true;
-  }
-  else{
+  } else {
     ErrorToast("Request Failed! Try Again");
     return false;
   }
 }
-
-
 
 // import 'dart:convert';
 // import 'package:http/http.dart' as http;
